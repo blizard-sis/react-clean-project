5432a9433b1a2e14dd641b0a11c5f670ebd2c2b1
Ilya Selenzev <blizard_sis@rambler.ru>
2024-11-01 10:08:15 +0300

    base files

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import style from './index.module.scss'
+import './index.scss'
+import './assets/images/dog.jpg'
+
+console.log(style)
+
+const numbers: number[] = [2, 3, 5]
+
+const doubledNumbers = numbers.map((number) => number * 2)
+
+console.log(doubledNumbers)
---
+++ webpack/webpack.dev.js
@@ -0,0 +1,17 @@
+const path = require('path'); 
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    devtool: 'eval-source-map',
+    devServer: {
+        static: path.resolve(__dirname, './dist'),
+        // compress: true,
+        port: 8080,
+        open: true,
+        hot: true,
+    },
+    plugins: [
+        new ReactRefreshWebpackPlugin(),
+    ],
+}
